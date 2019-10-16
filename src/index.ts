288759af650b5fff8f5231f7e4205290896174f7
kevinpollet <pollet.kevin@gmail.com>
2019-10-16 18:01:52 +0400

    Add pika build handlers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+import { BuilderOptions } from "@pika/types";
+
+export const beforeBuild = async (opts: BuilderOptions): Promise<void> =>
+  opts.reporter.info("beforeBuild");
+
+export const afterBuild = async (opts: BuilderOptions): Promise<void> =>
+  opts.reporter.info("beforeBuild");
+
+export const beforeJob = async (opts: BuilderOptions): Promise<void> =>
+  opts.reporter.info("beforeJob");
+
+export const afterJob = async (opts: BuilderOptions): Promise<void> =>
+  opts.reporter.info("beforeJob");
+
+export const build = async (opts: BuilderOptions): Promise<void> =>
+  opts.reporter.info("Build");
+
+export const manifest = async (
+  manifest: any,
+  opts: BuilderOptions
+): Promise<any> => {
+  opts.reporter.info(`Manifest: ${manifest}`);
+  return manifest;
+};
